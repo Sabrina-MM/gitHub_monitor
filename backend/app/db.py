69e6d89d0621529d797a04fd8ba9488b869557db
Sabrina <sabrinamarisolmartinez@gmail.com>
2025-02-20 18:10:34 +0000

    Config Render variables

--- backend/app/db.py
+++ backend/app/db.py
@@ -1,15 +1,18 @@
 # Conexión a MySQL
 
 import pymysql
+import os
 from pymysql.cursors import DictCursor
 
 def get_connection():
     connection = pymysql.connect(
-        host='127.0.0.1',      # Cambiar según configuración
-        user='root',     # Usuario de MySQL
-        password='root',# Contraseña de MySQL
-        db='github_monitor',  # Base de datos a utilizar
+        host=os.getenv("DB_HOST"),      # Cambiar según configuración
+        user=os.getenv("DB_USER"),     # Usuario de MySQL
+        password=os.getenv("DB_PASSWORD"),# Contraseña de MySQL
+        db=os.getenv("DB_NAME"),  # Base de datos a utilizar
+        port=int(os.getenv("DB_PORT")),  # Puerto MySQL
         charset='utf8mb4',
         cursorclass=DictCursor
     )
     return connection
+#mysql://root:KgkSfZkVDffALNNEsWtlJQaoeIUgAWRx@metro.proxy.rlwy.net:12335/railway
